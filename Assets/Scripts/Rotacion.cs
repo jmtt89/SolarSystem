4260186ba910fced18e7fe8b849848cb5826a232
Jesus <jmtt89@gmail.com>
2017-11-13 05:44:52 -0400

    First Commit

---
+++ Assets/Scripts/Rotacion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotacion : MonoBehaviour {
+    int factor = 12;
+    public long timeAround = 86400;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.down, 360.0f / (timeAround/factor) * Time.deltaTime, Space.World);
+    }
+}
---
+++ Assets/Scripts/Translacion.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Translacion : MonoBehaviour {
+    int factor = 12;
+    public long timeAround = 31536000;
+    public Transform origin;
+
+	// Use this for initialization
+	void Start () {	
+	}
+
+    // Update is called once per frame
+    void Update() {
+        transform.RotateAround(origin.position, Vector3.down, 360.0f / (timeAround/factor) * Time.deltaTime);
+	}
+}
